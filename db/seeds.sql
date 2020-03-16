55a655938110cd0a6f8b28ae81c5feeea4725d9c
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-15 22:34:16 -0700

    added the schema and seeds, then ran mysql database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- USE burgers_DB;
+
+INSERT INTO burgers (name, devoured) VALUES ('Le Pigeon', false);
+INSERT INTO burgers (name, devoured) VALUES ('Whiskey King', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Animal Style', false);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
